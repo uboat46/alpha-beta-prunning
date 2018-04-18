21f668cfdea27e55ebbb7dc6f1c3975419606ac2
Uboat46 <uboat46@hotmail.com>
2018-04-18 07:52:25 -0500

    KIND OF WORKS

--- run/run.js
+++ run/run.js
@@ -8,8 +8,8 @@ function runFile() {
               (1 4 3) (1 5 3) (1 6 3) (1 7 3)
               (1 8 3) (1 9 3) (1 10 3) (1 11 3)
               (NIL 12 0) (NIL 13 0) (NIL 14 0) (NIL 15 0)
-              (2 16 3) (NIL 17 0) (NIL 18 0) (NIL 19 0)
-              (NIL 20 0) (2 21 3) (2 22 3) (2 23 3)
+              (NIL 16 0) (NIL 17 0) (NIL 18 0) (NIL 19 0)
+              (2 20 3) (2 21 3) (2 22 3) (2 23 3)
               (2 24 3) (2 25 3) (2 26 3) (2 27 3)
               (2 28 3) (2 29 3) (2 30 3) (2 31 3)   )`;
   let pcomer = `'( (1 0 3) (1 1 3) (1 2 3) (1 3 3) 
@@ -22,7 +22,7 @@ function runFile() {
                 (2 28 3) (2 29 3) (2 30 3) (2 31 3)   )`;
               //PUEDEMOVER ${s} 8 1 3 
               //(JUEGA ${s} 1 6)
-  let ls = spawn('clisp', ['-q','-i', `${__dirname}/../alpha-beta/alpha-beta.fas`, '-x', `(JUEGA ${pcomer} 1 6)`]);
+  let ls = spawn('clisp', ['-q','-i', `${__dirname}/../alpha-beta/alpha-beta.fas`, '-x', `(JUEGA ${s} 1 11)`]);
   ls.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
   });
